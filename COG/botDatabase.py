e07beb94cc49f75a7bf1bed8e641fd45d8605876
Magnus H. <mfh1106@gmail.com>
2024-05-29 16:35:55 +0200

    changed punishments.csv to loreevents.csv

--- COG/botDatabase.py
+++ COG/botDatabase.py
@@ -14,29 +14,28 @@ class Database(commands.Cog):
         if os.path.exists(filepath) != True:
             os.makedirs(filepath)
 
-    async def loreEvent(guildID:int, date:int, loreEvent:str):
+    async def loreEvent(guildID:int, date:int, loreevent:str):
 
-        field_names = ['date', 'loreEvent']
+        field_names = ['date', 'loreevent']
         current_dir = os.getcwd()
         filepath = os.path.expanduser(os.path.join(current_dir, 'DB', str(guildID)))
         await Database.filepathExists(filepath=filepath)
 
 
-        if os.path.exists(filepath + '/punishments.csv') != True: # If './DB/GuildID/punishments.csv' doesn't exist 
+        if os.path.exists(filepath + '/loreevents.csv') != True: # If './DB/GuildID/loreevents.csv' doesn't exist 
             Database.filepathExists(filepath=filepath)
-            open(filepath+'\punishments.csv','x')
-            with open((os.path.join(filepath, 'punishments.csv')),'w', newline='') as csvfile: # Make a new csv fil
+            open(filepath+'\loreevents.csv','x')
+            with open((os.path.join(filepath, 'loreevents.csv')),'w', newline='') as csvfile: # Make a new csv fil
                 writer = csv.DictWriter(csvfile, fieldnames=field_names) # With field_names in the header (also creates header)
                 writer.writeheader()
-                writer.writerow({f'date': date,'loreEvent':loreEvent}) # Write case 1/loreevent 1
-                print(f'Made new .csv file for Guild {discord.Guild.name(guildID)} 
-                      \nWith GuildID: {guildID}')
+                writer.writerow({f'date': date,'loreevent':loreevent}) # Write case 1/loreevent 1
+                print(f'Made new .csv file for a Guild.\nWith the GuildID: {guildID}')
                 return 'New Guild'
 
-        else: # ... 'punishments.csv' exists!
-            with open(filepath + '/punishments.csv','a', newline='') as f_object: # open file in append mode!
+        else: # ... 'loreevents.csv' exists!
+            with open(filepath + '/loreevents.csv','a', newline='') as f_object: # open file in append mode!
                 dictwriter_object = DictWriter(f_object, fieldnames=field_names) # with the same fieldnames as before
-                dictwriter_object.writerow({f'date': date,'loreEvent':loreEvent}) # Write the new event to file
+                dictwriter_object.writerow({f'date': date,'loreevent':loreevent}) # Write the new event to file
                 print('Appended most recent case')
                 return 'Success'
 
